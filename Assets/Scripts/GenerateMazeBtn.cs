ae4058e0e4d33f13ff29a7fded675f8ccc87068a
UO276213 <uo276213@uniovi.es>
2023-08-16 16:24:26 +0200

    generate maze with depth-first search algorithm

---
+++ Assets/Scripts/GenerateMazeBtn.cs
@@ -0,0 +1,21 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+namespace Assets.Scripts
+{
+    [CustomEditor(typeof(MazeGenerator))]
+    public class GenerateMazeBtn : Editor
+    {
+        public override void OnInspectorGUI()
+        {
+            MazeGenerator mazeGenerator = (MazeGenerator) target;
+            DrawDefaultInspector();
+            if (GUILayout.Button("Generate"))
+            {
+                mazeGenerator.GenerateMaze();
+            }
+            base.OnInspectorGUI();
+            
+        }
+    }
+}
---
+++ Assets/Scripts/MazeDisplay.cs
@@ -0,0 +1,40 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+namespace Assets.Scripts
+{
+    public class MazeDisplay : MonoBehaviour
+    {
+        public Color color;
+
+        public Texture2D texture;
+        // public int textureWidth = 256;
+        // public int textureHeight = 256;
+        // public int scale = 1;
+
+        public static void DrawMaze(bool[,] maze)
+        {
+            int mazeHeight = maze.GetLength(0);
+            int mazeWidth = maze.GetLength(1);
+            var texture = new Texture2D(mazeWidth, mazeHeight);
+
+            Color[] colors = new Color[mazeWidth * mazeHeight];
+
+            for (int i = 0; i < mazeHeight; i++)
+            {
+                for (int j = 0; j < mazeWidth; j++)
+                {
+                    if (maze[i, j])
+                    {
+                        colors[i * mazeWidth + j] = Color.red;
+                    }
+                       
+                }
+            }
+            
+            texture.SetPixels(colors);
+            texture.Apply();
+            AssetDatabase.CreateAsset(texture, "Assets/MyMaterial.renderTexture");
+        }
+    }
+}
